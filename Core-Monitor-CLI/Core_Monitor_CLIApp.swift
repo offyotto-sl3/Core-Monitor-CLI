86dae4b8cc5da781e844492c0420fd41e5272d5d
ventaphobia <nazishamin65@gmail.com>
2026-04-14 16:41:19 +0500

    Initial Commit

---
+++ Core-Monitor-CLI/Core_Monitor_CLIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Core_Monitor_CLIApp.swift
+//  Core-Monitor-CLI
+//
+//  Created by bookme on 4/14/26.
+//
+
+import SwiftUI
+
+@main
+struct Core_Monitor_CLIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
